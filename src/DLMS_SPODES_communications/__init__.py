1f4d013e4fb2ce4b964a679490890e88077ecd4c
vasilsv <oe8v3s>
2024-01-19 13:27:51 +0300

    ver 1.0.1 __init__: argparser for script, import drivers

---
+++ src/DLMS_SPODES_communications/__init__.py
@@ -0,0 +1,15 @@
+from .network import Network
+from .ble import BLE
+from .serial_port import SerialPort, RS485
+# import argparse
+import sys
+
+
+def call_script(args=sys.argv):
+    for i, arg in enumerate(args):
+        print(f'Arg #{i}: {arg}')
+    # parser = argparse.ArgumentParser(description="hello")
+    # parser.add_argument('target', type=str, help='the name of the target')
+    # parser.add_argument('--end', dest='end', default="!",
+    #                     help='sum the integers (default: find the max)')
+    # args = parser.parse_args()
